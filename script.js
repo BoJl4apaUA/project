52adac352ec9af6d6bbcbfea218947f4ccef0383
Oleg <example@gamil.com>
2022-11-21 14:09:34 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const answers = [];
+
+
+ansers[0] = prompt('как ваше имя?', '');
+ansers[1] = prompt('как ваше фамилия?', '');
+ansers[2] = prompt('сколько вам лет?', '');
+
+console.log(typeof(answers));
